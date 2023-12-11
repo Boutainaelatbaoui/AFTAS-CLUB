3ea8826f6207f1955d0375a7c5884100cb04d96a
Boutaina <boutaina.elatbaoui@gmail.com>
2023-12-11 09:39:11 +0100

    Added README file

---
+++ AFTAS-backend/src/main/java/com/aftas/aftasbackend/AftasClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aftas.aftasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasClubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasClubBackendApplication.class, args);
+    }
+
+}
---
+++ AFTAS-backend/src/test/java/com/aftas/aftasbackend/AftasClubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aftas.aftasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasClubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
