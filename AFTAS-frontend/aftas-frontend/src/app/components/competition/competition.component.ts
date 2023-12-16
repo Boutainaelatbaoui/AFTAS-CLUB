530b9d88c4bfac28c40bc343fdef93d453370c49
Boutaina <boutaina.elatbaoui@gmail.com>
2023-12-16 13:49:41 +0100

    Added filter to competition

--- AFTAS-frontend/aftas-frontend/src/app/components/competition/competition.component.ts
+++ AFTAS-frontend/aftas-frontend/src/app/components/competition/competition.component.ts
@@ -9,22 +9,43 @@ import { CompetitionService } from 'src/app/services/competitionService/competit
 })
 export class CompetitionComponent implements OnInit {
   competitions: Competition[] = [];
+  filteredCompetitions: Competition[] = [];
+  selectedFilter: string = '';
   imageSection2: string = 'assets/img/Hunt2.jpg';
 
   constructor(private competitionService: CompetitionService) {}
 
   ngOnInit(): void {
-    this.getCompetitions();
+    this.competitionService.getAllCompetitions().subscribe((competitions) => {
+      this.competitions = competitions;
+      this.filteredCompetitions = competitions;
+    });
   }
 
-  getCompetitions(): void {
-    this.competitionService.getAllCompetitions().subscribe(
-      (competitions) => {
-        this.competitions = competitions;
-      },
-      (error) => {
-        console.error('Error fetching competitions:', error);
-      }
-    );
+  filterCompetitions(): void {
+    if (this.selectedFilter === 'en cours') {
+      this.filteredCompetitions = this.competitions.filter(
+        (competition) => this.isCompetitionInProgress(competition)
+      );
+    } else if (this.selectedFilter === 'fermé') {
+      this.filteredCompetitions = this.competitions.filter(
+        (competition) => !this.isCompetitionInProgress(competition)
+      );
+    } else {
+      this.filteredCompetitions = this.competitions;
+    }
   }
+
+  private isCompetitionInProgress(competition: Competition): boolean {
+    const currentDate = new Date();
+    const competitionDateTime = new Date(`${competition.date}T${competition.startTime}`);
+    return currentDate < competitionDateTime;
+  }
+
+  selectFilter(filter: string): void {
+    console.log('Filter selected:', filter);
+    this.selectedFilter = filter;
+    this.filterCompetitions();
+  }
+  
 }
